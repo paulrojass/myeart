192ce0969e3456538c0c704ec727065a42b51a8e
Chgf <chgf1997@gmail.com>
2021-06-27 14:31:26 -0400

    modificacion CardBIOArtist

--- resources/js/Components/CardBIOArtist.vue
+++ resources/js/Components/CardBIOArtist.vue
@@ -3,37 +3,54 @@
         <div class="product-thumb card-artist">
             <figure class="position-relative">
                 <img :src="item.photo" alt="" class="" />
-                <figcaption style="border-radius: 50%;">
+                <figcaption
+                    style="
+                        border-radius: 50%;
+                        width: 350px;
+                        height: 300px;
+                        cursor: pointer;
+                    "
+                >
                     <div
                         class="p-5 text-center d-flex flex-column justify-content-between"
-                        style="color: white; height: 100%;"
+                        style="color: white; height: 100%"
                     >
                         <div class="cardBIO-text">Artista 1</div>
                         <div class="cardBIO-text">Pintor</div>
                         <div class="d-flex justify-content-center">
-                            <div class="product-excerpt m-0 p-0 border-none ">
-                                <div class="d-flex align-items-center ">
+                            <div class="product-excerpt m-0 p-0 border-none">
+                                <div class="d-flex align-items-center">
                                     <div>
                                         <ul
                                             class="product-facts clearfix m-0 p-0 border-none position-relative"
-                                            style="width: 100px;"
+                                            style="width: 100px"
                                         >
                                             <li class="product-rating">
                                                 <ul class="list-unstyled">
                                                     <li>
-                                                        <span class="rate_active"></span>
+                                                        <span
+                                                            class="rate_active"
+                                                        ></span>
                                                     </li>
                                                     <li>
-                                                        <span class="rate_active"></span>
+                                                        <span
+                                                            class="rate_active"
+                                                        ></span>
                                                     </li>
                                                     <li>
-                                                        <span class="rate_active"></span>
+                                                        <span
+                                                            class="rate_active"
+                                                        ></span>
                                                     </li>
                                                     <li>
-                                                        <span class="rate_active"></span>
+                                                        <span
+                                                            class="rate_active"
+                                                        ></span>
                                                     </li>
                                                     <li>
-                                                        <span class="rate_disabled"></span>
+                                                        <span
+                                                            class="rate_disabled"
+                                                        ></span>
                                                     </li>
                                                 </ul>
                                             </li>
@@ -76,18 +93,18 @@ export default {
 </script>
 
 <style scoped>
-    .cardBIO-text {
-        font-weight: bold;
-        font-size: 18px;
-    }
+.cardBIO-text {
+    font-weight: bold;
+    font-size: 18px;
+}
 
-    .product-thumb.product-thumb {
-        overflow: revert;
-    }
+.product-thumb.product-thumb {
+    overflow: revert;
+}
 
-    .product-single .product-thumb figure img {
-        border-radius: 50%;
-        width: 100%;
-    }
-
+.product-single .product-thumb figure img {
+    border-radius: 50%;
+    width: 350px;
+    height: 300px;
+}
 </style>
